1263e28ef0c44a51a75db0cb1c17e9b5f8e9a7b1
MajdeddineLetaief <majdeddine.letaief@gmail.com>
2018-02-09 15:23:12 +0100

    my first commit in the new project

---
+++ testProjectx/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/projectx/Documents/Work/Projects/projectxProject/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
